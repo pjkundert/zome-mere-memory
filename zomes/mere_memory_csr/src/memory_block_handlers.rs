726f7b56143146ef9ab4f6d900113ac9e770cd93
Perry Kundert <perry@dominionrnd.com>
2025-01-22 07:16:18 -0700

    Get Entry hash from Action instead of re-hashing, improve Makefile

--- zomes/mere_memory_csr/src/memory_block_handlers.rs
+++ zomes/mere_memory_csr/src/memory_block_handlers.rs
@@ -5,16 +5,24 @@ use crate::{
 
 use hdk::prelude::*;
 
+fn get_entry_hash_for_action_hash_local(action_hash: &ActionHash, options: GetOptions) -> ExternResult<Option<EntryHash>> {
+    Ok(get_details(action_hash.to_owned(), options)?
+       .and_then(|details| match details {
+	   Details::Record(record_details) => record_details.record.action().entry_hash().cloned(),
+	   _ => None,
+       }))
+}
+
 
 #[hdk_extern]
 pub fn create_memory_block_entry(block: MemoryBlockEntry) -> ExternResult<EntryHash> {
     debug!("Creating 'MemoryBlockEntry' ({}/{}): {}", block.sequence.position, block.sequence.length, block.bytes.len() );
+    let action_hash = create_entry( block.to_input() )?;
+    let entry_hash = get_entry_hash_for_action_hash_local( &action_hash, GetOptions::local() )?
+        .ok_or(wasm_error!(WasmErrorInner::Guest(format!("Entry hash not found for Action: {}", action_hash ))))?;
 
-    create_entry( block.to_input() )?;
-
-    Ok( hash_entry( &block )? )
+    Ok(entry_hash)
 }
-
 
 
 #[hdk_extern]
